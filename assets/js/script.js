35a3652ac36f40370d1c33943c784d22ff11bfbb
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-03-25 23:47:00 -0400

    added current time to currentDay id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
-// const currentTime = document.querySelector("#currentDay");
+$(document).ready(function () {
+  //display current day and time
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  
+  });
 
-// const showCurrentTime = 
+  
